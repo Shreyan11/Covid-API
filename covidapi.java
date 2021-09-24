aaacec5fcd3700b0738bc64255b6f34d34a00a9f
Shreyan Jain <shreyannjain@gmail.com>
2021-09-24 21:32:38 +0530

    Create covidapi.java

---
+++ covidapi.java
@@ -0,0 +1,13 @@
+package app3.demo55;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class covidapi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(covidapi.class, args);
+	}
+
+}
