6172703586277bedfa3d65b0e0b2576bbc6aa266
Shreyan Jain <shreyannjain@gmail.com>
2021-09-28 22:41:40 +0530

    Simple API
    Just a test API to see if it works with the system

--- covidapi.java
+++ covidapi.java
@@ -1,4 +1,3 @@
-package app3.demo55;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,3 +10,107 @@ public class covidapi {
 	}
 
 }
+class Coffee{
+	private final String id;
+	private String name;
+	
+	public Coffee(String id,String name){
+		this.id=id;
+		this.name=name;
+	}
+	
+	public Coffee(String name){
+		this(UUID.randomUUID.toString(),name);
+	}
+	
+	
+	public String getName(){
+		return name;
+	}
+	
+	
+	public void setName(String name){
+		this.name= name;
+	}
+	
+}
+
+	
+@RestController
+class RestApiDemoController {
+	private List<Coffee> coffees = new ArrayList<>();
+	public RestApiDemoController() {
+	coffees.addAll(List.of(
+		new covidapi("Espresso"); 
+		new covidapi("Latte");
+		new covidapi("Ghana");
+		new covidapi("Columbia");
+			 ));
+	}
+	
+	
+	@GetMapping
+	Iterable<Coffee> getCoffees() {
+		return coffees;
+	}
+	
+	@GetMapping("/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		for (Coffee c: coffees) {
+		if (c.getId().equals(id)) {
+		return Optional.of(c);
+	}
+}
+	return Optional.empty();
+}
+	@PostMapping
+	Coffee postCoffee(@RequestBody Coffee coffee) {
+		coffees.add(coffee);
+		return coffee;
+}
+	@PutMapping("/{id}")
+	Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+		int coffeeIndex = -1;
+		for (Coffee c: coffees) {
+		if (c.getId().equals(id)) {
+		coffeeIndex = coffees.indexOf(c);
+		coffees.set(coffeeIndex, coffee);
+	}
+}
+	return (coffeeIndex == -1) ? postCoffee(coffee) : coffee;
+	}
+	@DeleteMapping("/{id}")
+		void deleteCoffee(@PathVariable String id) {
+		coffees.removeIf(c -> c.getId().equals(id));
+}
+}
+
+	@PutMapping("/{id}")
+		ResponseEntity<Coffee> putCoffee(@PathVariable String id,
+		@RequestBody Coffee coffee) {
+		int coffeeIndex = -1;
+		for (Coffee c: coffees) {
+		if (c.getId().equals(id)) {
+		coffeeIndex = coffees.indexOf(c);
+		coffees.set(coffeeIndex, coffee);
+}
+}
+	return (coffeeIndex == -1) ?
+	new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
+	new ResponseEntity<>(coffee, HttpStatus.OK);
+}
+	
+			 
+		
+		
+		
+
+
+
+
+
+
+
+
+
+
